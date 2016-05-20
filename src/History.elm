c391a3054e528ea7a41c1bc2c1809e82626d9fb1
Kris Jenkins <krisajenkins@gmail.com>
2016-05-20 16:55:48 +0100

    Initial commit.

---
+++ src/History.elm
@@ -0,0 +1,58 @@
+module History exposing (Router, Path, create)
+
+{-| Manage browser history and link creation using the HTML5 history API.
+
+@docs Router, Path, create
+-}
+
+import Exts.String exposing (removePrefix)
+import String exposing (split, join)
+
+
+{-| A `Router` is a function which can turn your `view` types into strings.
+
+
+    a [href (router (AccountView accountnumber))] [...]
+
+-}
+type alias Router view =
+    view -> String
+
+
+type alias Parser view =
+    String -> view
+
+
+{-| A path is just a URI split up into sections. For example
+`/account/1234/settings` would convert to the path
+`["account", "1234", "settings"]`.
+-}
+type alias Path =
+    List String
+
+
+type alias History view =
+    { parser : String -> view
+    , router : view -> String
+    }
+
+
+{-| Create a history manager by supplying one function that goes from
+`Path` to `view`, and one that goes the other way from `view` to
+`Path`.
+-}
+create : (Path -> view) -> (view -> Path) -> History view
+create fromPath toPath =
+    { parser = fromUri fromPath
+    , router = toUri toPath
+    }
+
+
+fromUri : (Path -> view) -> Parser view
+fromUri fromPath =
+    removePrefix "#/" >> split "/" >> fromPath
+
+
+toUri : (view -> Path) -> Router view
+toUri toPath =
+    toPath >> join "/" >> (++) "#/"
---
+++ test/Main.elm
@@ -0,0 +1,13 @@
+module Main exposing (..)
+
+import ElmTest exposing (..)
+
+
+tests : Test
+tests =
+  suite "All" [ ]
+
+
+main : Program Never
+main =
+  runSuite tests
